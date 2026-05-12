7801bd305486f5fa78dc5cba32de87115edbaffa
Ashwinishree M <ashwinishreem@gmail.com>
2026-05-12 15:25:07 +0530

    Refactor server.js to use dotenv and enable CORS
    Added dotenv for environment variable management and enabled CORS.

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 app.get('/pipeline-status', async (req, res) => {
   try {
     const response = await axios.get(
       'https://api.github.com/repos/Ashwinishree08/ci-cd-monitor/actions/runs',
-      { headers: { Authorization: `token ghp_ABC123xyz...` } }
+      { headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` } }
     );
     res.json(response.data);
   } catch (error) {
